8d03bef9b598d90aed723d42bcbe45a9edeff18c
mmveres <mmveres@gmail.com>
2020-12-05 15:22:26 +0200

    lesson01

---
+++ ua/univer/lesson01/task02.py
@@ -0,0 +1,7 @@
+x = int(input("Enter x"))
+y = int(input("Enter y"))
+
+x+=1
+z = x + y
+print(type(z))
+print(x, " + ", y, " = ", z)
---
+++ ua/univer/lesson01/task03.py
@@ -0,0 +1,6 @@
+age = int(input("Enter age"))
+hour = int(input("Enter time hour"))
+can_by_alcohol = age>18 and hour<22
+print("can_by_alcohol = ",can_by_alcohol)
+can_by_sigar = age>18
+print("can_by_sigar = ",can_by_sigar)
